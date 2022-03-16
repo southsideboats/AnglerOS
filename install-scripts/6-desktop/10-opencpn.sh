60c199f3c60a1ebfdd213cfae4bf6416349ebbbb
mgrouch <mgrushinskiy@gmail.com>
2022-03-16 19:54:03 -0400

    mini

--- install-scripts/6-desktop/10-opencpn.sh
+++ install-scripts/6-desktop/10-opencpn.sh
@@ -47,71 +47,71 @@
 #  rm -rf o-tmp
 #fi
 
-if [ $LMARCH == 'armhf' ]; then
-  apt-get install -y -q                                         \
-    oernc-pi                                                    \
-    oesenc-pi                                                   \
-    opencpn-doc                                                 \
-    opencpn-plugin-calculator                                   \
-    opencpn-plugin-celestial                                    \
-    opencpn-plugin-chartscale                                   \
-    opencpn-plugin-climatology                                  \
-    opencpn-plugin-climatology-data                             \
-    opencpn-plugin-iacfleet                                     \
-    opencpn-plugin-launcher                                     \
-    opencpn-plugin-logbookkonni                                 \
-    opencpn-plugin-nmeaconverter                                \
-    opencpn-plugin-objsearch                                    \
-    opencpn-plugin-ocpndebugger                                 \
-    opencpn-plugin-plots                                        \
-    opencpn-plugin-pypilot                                      \
-    opencpn-plugin-radar                                        \
-    opencpn-plugin-s63                                          \
-    opencpn-plugin-sar                                          \
-    opencpn-plugin-tactics                                      \
-    opencpn-plugin-vfkaps                                       \
-    opencpn-plugin-watchdog                                     \
-    opencpn-plugin-weatherfax                                   \
-    opencpn-plugin-weatherrouting                               \
-    opencpn-plugin-draw                                         \
-    opencpn-sglock-arm32
-
-#  apt-get install -y -q opencpn-tcdata                         \
-#    opencpn-plugin-oernc                                       \
-#    opencpn-plugin-oesenc
-fi
-
-# Install plugin bundle
-mkdir tmp-o-bundle-$LMARCH || exit 2
-cd tmp-o-bundle-$LMARCH
-
-wget -O opencpn-plugins-bundle-$LMARCH.tar.gz https://github.com/bareboat-necessities/opencpn-plugins-bundle/blob/main/bundles/opencpn-plugins-bundle-$LMARCH.tar.gz?raw=true
-gzip -cd opencpn-plugins-bundle-$LMARCH.tar.gz | tar xvf -
-
-cp -r -p lib/* /usr/lib/
-cp -r -p bin/* /usr/bin/
-cp -r -p share/* /usr/share/
-
-cd ..
-rm -rf tmp-o-bundle-$LMARCH
-
-mv /usr/lib/opencpn/libPolar_pi.so /usr/lib/opencpn/libpolar_pi.so
-
-if [ -f /usr/lib/opencpn/liblogbookkonni_pi.so ]
-then
-  rm -f /usr/lib/opencpn/libLogbookKonni_pi.so
-fi
-
-# Polar Diagrams
-
-BK_DIR="$(pwd)"
-
-mkdir /home/user/Polars && cd /home/user/Polars
-
-wget https://www.seapilot.com/wp-content/uploads/2018/05/All_polar_files.zip
-unzip All_polar_files.zip
-chown user:user ./*
-chmod 664 ./*
-rm All_polar_files.zip
-
-cd "$BK_DIR"
+#if [ $LMARCH == 'armhf' ]; then
+#  apt-get install -y -q                                         \
+#    oernc-pi                                                    \
+#    oesenc-pi                                                   \
+#    opencpn-doc                                                 \
+#    opencpn-plugin-calculator                                   \
+#    opencpn-plugin-celestial                                    \
+#    opencpn-plugin-chartscale                                   \
+#    opencpn-plugin-climatology                                  \
+#    opencpn-plugin-climatology-data                             \
+#    opencpn-plugin-iacfleet                                     \
+#    opencpn-plugin-launcher                                     \
+#    opencpn-plugin-logbookkonni                                 \
+#    opencpn-plugin-nmeaconverter                                \
+#    opencpn-plugin-objsearch                                    \
+#    opencpn-plugin-ocpndebugger                                 \
+#    opencpn-plugin-plots                                        \
+#    opencpn-plugin-pypilot                                      \
+#    opencpn-plugin-radar                                        \
+#    opencpn-plugin-s63                                          \
+#    opencpn-plugin-sar                                          \
+#    opencpn-plugin-tactics                                      \
+#    opencpn-plugin-vfkaps                                       \
+#    opencpn-plugin-watchdog                                     \
+#    opencpn-plugin-weatherfax                                   \
+#    opencpn-plugin-weatherrouting                               \
+#    opencpn-plugin-draw                                         \
+#    opencpn-sglock-arm32
+#
+##  apt-get install -y -q opencpn-tcdata                         \
+##    opencpn-plugin-oernc                                       \
+##    opencpn-plugin-oesenc
+#fi
+#
+## Install plugin bundle
+#mkdir tmp-o-bundle-$LMARCH || exit 2
+#cd tmp-o-bundle-$LMARCH
+#
+#wget -O opencpn-plugins-bundle-$LMARCH.tar.gz https://github.com/bareboat-necessities/opencpn-plugins-bundle/blob/main/bundles/opencpn-plugins-bundle-$LMARCH.tar.gz?raw=true
+#gzip -cd opencpn-plugins-bundle-$LMARCH.tar.gz | tar xvf -
+#
+#cp -r -p lib/* /usr/lib/
+#cp -r -p bin/* /usr/bin/
+#cp -r -p share/* /usr/share/
+#
+#cd ..
+#rm -rf tmp-o-bundle-$LMARCH
+#
+#mv /usr/lib/opencpn/libPolar_pi.so /usr/lib/opencpn/libpolar_pi.so
+#
+#if [ -f /usr/lib/opencpn/liblogbookkonni_pi.so ]
+#then
+#  rm -f /usr/lib/opencpn/libLogbookKonni_pi.so
+#fi
+#
+## Polar Diagrams
+#
+#BK_DIR="$(pwd)"
+#
+#mkdir /home/user/Polars && cd /home/user/Polars
+#
+#wget https://www.seapilot.com/wp-content/uploads/2018/05/All_polar_files.zip
+#unzip All_polar_files.zip
+#chown user:user ./*
+#chmod 664 ./*
+#rm All_polar_files.zip
+#
+#cd "$BK_DIR"
